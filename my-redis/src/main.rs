f7c0687b82434f69d51141a2828b5d074148783f
Kingfree <kingfree@toyama.moe>
2020-11-27 13:39:15 +0800

    store values for per process

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -13,10 +13,28 @@ async fn main() {
 }
 
 async fn process(socket: TcpStream) {
+    use mini_redis::Command::{self, Get, Set};
+    use std::collections::HashMap;
+
+    let mut db = HashMap::new();
+
     let mut conn = Connection::new(socket);
-    if let Some(frame) = conn.read_frame().await.unwrap() {
-        println!("收到: {:?}", frame);
-        let response = Frame::Error("未实现".to_string());
+
+    while let Some(frame) = conn.read_frame().await.unwrap() {
+        let response = match Command::from_frame(frame).unwrap() {
+            Set(cmd) => {
+                db.insert(cmd.key().to_string(), cmd.value().clone());
+                Frame::Simple("OK".to_string())
+            }
+            Get(cmd) => {
+                if let Some(value) = db.get(cmd.key()) {
+                    Frame::Bulk(value.clone())
+                } else {
+                    Frame::Null
+                }
+            }
+            cmd => panic!("未实现 {:?}", cmd),
+        };
         conn.write_frame(&response).await.unwrap();
     }
 }
