6616894b22618f06dfcc484f5512d297ab7f823f
Kingfree <kingfree@toyama.moe>
2020-11-27 15:48:31 +0800

    impl Future

---
+++ my-redis/examples/future.rs
@@ -0,0 +1,31 @@
+use std::future::Future;
+use std::pin::Pin;
+use std::task::{Context, Poll};
+use std::time::{Duration, Instant};
+
+struct Delay {
+    when: Instant,
+}
+
+impl Future for Delay {
+    type Output = &'static str;
+
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if Instant::now() >= self.when {
+            println!("時は来た");
+            Poll::Ready("done")
+        } else {
+            cx.waker().wake_by_ref();
+            println!("時を待っている");
+            Poll::Pending
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let when = Instant::now() + Duration::from_millis(10);
+    let future = Delay { when };
+    let out = future.await;
+    assert_eq!(out, "done");
+}
