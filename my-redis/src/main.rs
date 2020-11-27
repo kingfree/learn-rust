60973dd0245bf6b87641f87350cfa2c3c723b910
Kingfree <kingfree@toyama.moe>
2020-11-27 13:31:54 +0800

    concurrency

--- my-redis/src/main.rs
+++ my-redis/src/main.rs
@@ -6,7 +6,9 @@ async fn main() {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
     loop {
         let (socket, _) = listener.accept().await.unwrap();
-        process(socket).await;
+        tokio::spawn(async move {
+            process(socket).await;
+        });
     }
 }
 
